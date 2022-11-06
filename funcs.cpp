b0c4876806e6232faa9ee875288979c1374f6d81
JianHua-Deng <dengjianhua2397462613@gmail.com>
2022-11-06 18:26:40 -0500

    Added Task A-E, stil need tests

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <cmath>
+#include "coord3d.h"
 #include "funcs.h"
 
 // add functions here
+//task A
+double length(Coord3D *p){
+    return sqrt(pow((*p).x,2) + pow((*p).y,2) + pow((*p).z,2));
+}//end length function
+
+//Task B
+Coord3D *fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+    if(length(p1) > length(p2)){
+        return p1;
+    }else{
+        return p2;
+    }//end else condition
+}//end fartherFromOrigin function
+
+//Task C
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+    (*ppos).x = (*ppos).x + (*pvel).x * dt;
+    (*ppos).y = (*ppos).y + (*pvel).y * dt;
+    (*ppos).z = (*ppos).z + (*pvel).z * dt;
+}//end move function
+
+//Task E
+Coord3D* createCoord3D(double x, double y, double z){
+    Coord3D *newobject = new Coord3D; 
+    (*newobject).x = x;
+    (*newobject).y = y;
+    (*newobject).z = z;
+    return newobject;
+}//end createCoord3D function
+
+void deleteCoord3D(Coord3D *p){
+    delete p;
+}//end deleteCoord3D function
+
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
-
+#include "coord3d.h"
 #include "funcs.h"
 
 int main()
 {
+  Coord3D pointP = {10, 20, 30};
+  Coord3D pointQ = {-20, 21, -22};
+
+  Coord3D pos = {0, 0, 100.0};
+  Coord3D vel = {1, -5, 0.2};
+
+  std::cout << length(&pointP) << std::endl;
+  Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+  std::cout << "Address of pointP: " << &pointP << "\n" << "Adress of pointQ: " << &pointQ << "\n" << "Answer: " << ans << "\n";
+
+  move(&pos, &vel, 2.0);
+  std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
+
+
   return 0;
-}
+}//end main
